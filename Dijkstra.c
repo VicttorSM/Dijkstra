6d5efbabf595bda1a9968bcc937e1fb3fb4d5b87
Victtor da Silva Mendes <33004275+VicttorSM@users.noreply.github.com>
2018-04-19 15:38:40 -0300

    Add files via upload

--- Dijkstra.c
+++ Dijkstra.c
@@ -6,30 +6,23 @@
 #define true 1
 #define false 0
 
-//FunÃ§Ã£o que conta o nÃºmero de colunas da matriz
+//Função que conta o número de colunas da matriz
 int numDeColunas(FILE *f){
 	int vertices = 0;
-	char str[1024];
-	fgets(str, 1024, f);
-	int i=0;
-	for(i=0; str[i]!='\0'; i++){
-		if(i==0){
-			if(str[i]!=' '){
-				vertices++;
-			}
-		}
-		else if(str[i]!=' '){
-			if(str[i-1]==' '){
-				vertices++;
-			}
-		}
+	char str[2048];
+	fgets(str, 2048, f);
+	int i=0, digitos, num, total=0;
+	while(sscanf(str+total, "%d%n", &num, &digitos)!=-1){
+		vertices++;
+		total+=digitos;
 	}
 	return vertices;
 }
 
-//FunÃ§Ã£o que imprime matriz
-void imprimeMatriz(int** m, int linhas, int colunas){
+//Função que imprime matriz
+void imprimeMatriz(int** m, int linhas, int colunas, char c[100]){
 	int i, j;
+	printf("Matriz de %s:\n", c);
 	for(i=0; i<linhas; i++){
 		char zero = '0';
 		for(j=0; j<colunas; j++){
@@ -39,9 +32,10 @@ void imprimeMatriz(int** m, int linhas, int colunas){
 		}
 		puts("");
 	}
+	puts("");
 }
 
-//FunÃ§Ã£o que adiciona um elemento na fila
+//Função que adiciona um elemento na fila
 void push(int* vec, int num, int size){
 	int i;
 	for(i=0; i<size; i++){
@@ -53,7 +47,7 @@ void push(int* vec, int num, int size){
 	}
 }
 
-//FunÃ§Ã£o que retira um elemento da fila
+//Função que retira um elemento da fila
 void pop(int* vec, int size){
 	int i;
 	for(i=0; i<size; i++){
@@ -62,55 +56,97 @@ void pop(int* vec, int size){
 	}
 }
 
-//FunÃ§Ã£o que cria matriz de distancias minimas
+//Função que lê matriz do arquivo f, e guarda nas matrizes pesos e d
+void lerMatriz(FILE *f, int linhas, int colunas, int** pesos, int** d){
+	int i, j;
+	for(i=0; i<linhas; i++){
+		for(j=0; j<colunas; j++){
+			fscanf(f, "%d", &pesos[i][j]);
+			if(pesos[i][j]==-1)pesos[i][j] = INFINITO;   //Se o numero for -1 o peso é infinito
+			d[i][j] = pesos[i][j];
+		}
+	}
+}
+
+//Função que imprime o caminho minimo entre dois vertices lidos do arquivo f
+void caminhoMinimo(FILE *f, int** d){
+	int u, v;
+	while(fscanf(f, "%d %d", &u, &v)==2){
+		if(d[u][v]==INFINITO)
+			printf("Nao eh possivel ir de %d ate %d\n", u, v);
+		else
+			printf("A distancia minima de %d ate %d eh: %d\n", u, v, d[u][v]);
+	}
+}
+
+//Função que aloca espaço para uma matriz
+int** alocaMatriz(int linhas, int colunas){
+	int** m;
+	m = malloc(sizeof(int*)*linhas);
+	int i;
+	for(i=0; i<linhas; i++){
+		m[i] = malloc(sizeof(int)*colunas);
+	}
+	return m;
+}
+
+//Função que destroi uma matriz
+void freeMatriz(int** m, int linhas){
+	int i;
+	for(i=0; i<linhas; i++){
+		free(m[i]);
+	}
+	free(m);
+}
+
+//Função que cria matriz de distancias minimas
 void dijkstra(int** g, int** d, int size){
 	int i;
 	int inicio;
-	//Aloca espaÃ§o na memoria para o vetor cor e Q
+	//Aloca espaço na memoria para o vetor cor e Q
 	int* cor = malloc(sizeof(int)*size);
 	int* Q = malloc(sizeof(int)*size);
 
 	/*
 	Repete o processo de achar a distancia minima
 	Uma vez para cada vertice
-	Inicio Ã© o vertice de ponto de partida
+	Inicio é o vertice de ponto de partida
 	*/
 	for(inicio=0; inicio<size; inicio++){
 		//Inicializa a fila Q e o vetor cor
 		for(i=0; i<size; i++){
-			Q[i] = -1; //-1 Ã© uma posiÃ§Ã£o da fila vazia
-			cor[i] = false; //Todo numero false ainda nÃ£o foi encontrado
+			Q[i] = -1; //-1 é uma posição da fila vazia
+			cor[i] = false; //Todo numero false ainda não foi encontrado
 		}
 
 		push(Q, inicio, size);	//Adiciona o numero inicial a fila Q
 		cor[inicio] = true;		//Marca o numero inicial como achado
-		//d[inicio][inicio] = 0;  //Define a distancia do numero inicial atÃ© ele mesmo como 0
 
-		//Enquanto a fila nÃ£o estiver vazia
+		//Enquanto a fila não estiver vazia
 		while(Q[0]!=-1){
-			int u = Q[0];   //Armazena o elemento da frente da fila na variÃ¡vel u
+			int u = Q[0];   //Armazena o elemento da frente da fila na variável u
 			pop(Q, size); 	//Retira um elemento da fila
 			for(i=0; i<size; i++){
-				int peso = g[u][i]; //Armazena o peso de u Ã  i
+				int peso = g[u][i]; //Armazena o peso de u à i
 
 				//Se o peso do vertice for infinito
 				if(peso==INFINITO)
 					continue;   //Ignora essa passagem do for
 
-				//Se a distancia atÃ© u + o peso for menor que a distancia atÃ© i atual
+				//Se a distancia até u + o peso for menor que a distancia até i atual
 				if(d[inicio][u] + peso < d[inicio][i]){
-					d[inicio][i] = d[inicio][u] + peso; //Troca a distancia atÃ© i atual pela soma do peso e distancia atÃ© u
+					d[inicio][i] = d[inicio][u] + peso; //Troca a distancia até i atual pela soma do peso e distancia até u
 				}
 
-				//Se o nÃºmero i ainda nÃ£o foi encontrado
+				//Se o número i ainda não foi encontrado
 				if(cor[i] == false){
-					cor[i] = true;  	//Marca o nÃºmero i como encontrado
+					cor[i] = true;  	//Marca o número i como encontrado
 					push(Q, i, size);   //Adiciona i a fila Q
 				}
 			}
 		}
 	}
-	//Liberta os espaÃ§os alocados da memÃ³ria
+	//Liberta os espaços alocados da memória
 	free(cor);
 	free(Q);
 }
@@ -119,66 +155,34 @@ int main()
 {
 	int vertices=0;
 	int x, y, z;
-	int *vec_pesos, *vec_d;
 	int **pesos, **d;
 
 	//Abre o arquivo
 	FILE *f = fopen("Entrada.txt", "r");
 	if(f==NULL) puts("ERRO! O arquivo nao foi aberto!");
-	else vertices = numDeColunas(f); //Pega o numero de vertices
-
-	rewind(f);  //Volta para o comeÃ§o do arquivo
-
-	//Aloca espaÃ§o para a matriz de pesos
-	vec_pesos = malloc(sizeof(int)*(vertices*vertices));
-	pesos = malloc(sizeof(int)*vertices);
-
-	//Aloca espaÃ§o para a matriz de distancias minimas
-	vec_d = malloc(sizeof(int)*(vertices*vertices));
-	d = malloc(sizeof(int)*vertices);
-	int k=0;
-
-	int i, j;
-	//Inicializa as matrizes
-	for(i=0; i<vertices; i++){
-		pesos[i] = vec_pesos+k;
-		d[i] = vec_d+k;
-		for(j=0; j<vertices; j++){
-			fscanf(f, "%d", &vec_pesos[k]);
-			if(vec_pesos[k]==-1)vec_pesos[k] = INFINITO;   //Se o numero for -1 o peso Ã© infinito
-			vec_d[k] = vec_pesos[k];
-			k++;
-		}
+	else{
+		vertices = numDeColunas(f); //Pega o numero de vertices
+		rewind(f);  //Volta para o começo do arquivo
 	}
-	//IMPRIME a matriz de pesos
-	puts("Matriz de pesos:");
-	imprimeMatriz(pesos, vertices, vertices);
-	puts("");
 
-	//Chama a funÃ§Ã£o de dijkstra
-	dijkstra(pesos, d, vertices);
+	//Aloca espaço para as matrizes
+	pesos = alocaMatriz(vertices, vertices);
+	d = alocaMatriz(vertices, vertices);
 
-	//IMPRIME a matriz de distancias minimas
-	puts("Matriz de distancia minima:");
-	imprimeMatriz(d, vertices, vertices);
-	puts("");
+	lerMatriz(f, vertices, vertices, pesos, d);	//Inicializa as matrizes
+	
+	imprimeMatriz(pesos, vertices, vertices, "pesos"); //IMPRIME a matriz de pesos
 
-	int u, v;
-	while(fscanf(f, "%d %d", &u, &v)==2){
-		if(d[u][v]==INFINITO)
-			printf("Nao eh possivel ir de %d ate %d\n", u, v);
-		else
-			printf("A distancia minima de %d ate %d eh: %d\n", u, v, d[u][v]);
-	}
+	dijkstra(pesos, d, vertices); //Chama a função de dijkstra
+
+	imprimeMatriz(d, vertices, vertices, "distancia minima"); //IMPRIME a matriz de distancias minimas
+
+	caminhoMinimo(f, d); //IMPRIME caminho minimo de um vertice a outro
 
 	fclose(f); //Fecha o arquivo
-
-	//Liberta os espaÃ§os alocados da memÃ³ria
-	free(vec_pesos);
-	free(pesos);
-
-	free(vec_d);
-	free(d);
+	//Liberta os espaços alocados da memória
+	freeMatriz(pesos, vertices);
+	freeMatriz(d, vertices);
 
 	system("pause");
 	return 0;
